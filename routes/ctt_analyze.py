38882bc92c3dc6110dfcb8be2b10dbadcf9ec176
longtruong2604 <longtruong2604@gmail.com>
2024-12-21 16:45:19 +0700

    feat: refactor and add CTT related API routes

--- routes/ctt_analyze.py
+++ routes/ctt_analyze.py
@@ -1,8 +1,9 @@
 import logging
 from flask import Blueprint, request, jsonify
-from services.analyze import analyze_uploaded_file
+from services.analyze import CttService
 
 ctt_analyze = Blueprint("ctt_analyze", __name__)
+ctt_service = CttService()
 
 
 @ctt_analyze.route("/ctt", methods=["POST"])
@@ -18,15 +19,89 @@ def analyze_file():
 
     try:
         # Delegate to the service layer
-        result = analyze_uploaded_file(file)
-        return jsonify(result), 200
+        ctt_service.analyze_uploaded_file(file)
+
+        return jsonify(
+            {
+                "message": "File uploaded and processed successfully.",
+                "data": "asb2s",
+            }
+        ), 200
 
     except FileNotFoundError as e:
         logging.error(f"File not found: {str(e)}")
-        return jsonify({"error": "Required file missing"}), 404
+        return jsonify({"message": "Required file missing"}), 404
     except ValueError as e:
         logging.error(f"Value error: {str(e)}")
-        return jsonify({"error": "Invalid file content"}), 400
+        return jsonify({"message": "Invalid file content"}), 400
     except Exception as e:
         logging.error(f"Unexpected error: {str(e)}")
-        return jsonify({"error": "An unexpected error occurred"}), 500
+        return jsonify({"message": "An unexpected error occurred"}), 500
+
+
+@ctt_analyze.route("/ctt/<analysis_id>", methods=["GET"])
+def get_analysis_results(analysis_id):
+    """
+    Endpoint to retrieve the analysis results.
+    """
+    try:
+        result = ctt_service.get_analysis_results()
+        return jsonify(
+            {
+                "message": f"Analysis {analysis_id} results retrieved successfully.",
+                "data": result,
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+
+@ctt_analyze.route("/ctt/<analysis_id>/general-detail", methods=["GET"])
+def get_general_detail(analysis_id):
+    """
+    Endpoint to retrieve general details about the analysis.
+    """
+    try:
+        general_detail = ctt_service.get_general_detail()
+        return jsonify(
+            {
+                "message": f"Analysis {analysis_id} general detail retrieved successfully.",
+                "data": general_detail,
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+
+@ctt_analyze.route("/ctt/question/<int:question_id>", methods=["GET"])
+def get_question_stats(question_id):
+    """
+    Endpoint to retrieve stats for a specific question.
+    """
+    try:
+        question_stats = ctt_service.get_question_stats(question_id)
+        return jsonify(
+            {
+                "message": "Question stats retrieved successfully.",
+                "data": question_id,
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+
+@ctt_analyze.route("/ctt/<analysis_id>/average-detail", methods=["GET"])
+def get_average_indexes(analysis_id):
+    """
+    Endpoint to retrieve average indexes from the analysis.
+    """
+    try:
+        average_indexes = ctt_service.get_average_indexes()
+        return jsonify(
+            {
+                "message": f"{analysis_id} average indexes retrieve successfully.",
+                "data": average_indexes,
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
--- services/analyze.py
+++ services/analyze.py
@@ -1,117 +1,267 @@
+# import os
+# from itertools import groupby
+
+# import pandas as pd
+
+# from config import UPLOAD_FOLDER
+# from models.exam import Exam
+# from models.exam_result import ExamResult
+# from models.question import Option, QuestionBank
+# from utils.data_processing import DataProcessing
+# from utils.file_handling import save_uploaded_file
+# from analysis.ctt_analysis import CttAnalysis
+
+
+# def analyze_uploaded_file(file):
+#     # Save and process the uploaded file
+#     file_path = save_uploaded_file(file)
+
+#     # Load Question Bank
+#     question_bank = QuestionBank()
+#     question_bank_df = pd.read_csv(
+#         os.path.join(UPLOAD_FOLDER, "mock_question_bank_items.csv")
+#     )
+#     question_bank_data = question_bank_df.to_dict(orient="records")
+
+#     for question in question_bank_data:
+#         question_id = question["Question_ID"]
+#         question_content = question["Content"]
+#         options = [
+#             Option(question["Option_A"]),
+#             Option(question["Option_B"]),
+#             Option(question["Option_C"]),
+#             Option(question["Option_D"]),
+#         ]
+#         correct_answer = question["Correct_Option"]
+#         correct_answer_index = ["A", "B", "C", "D"].index(correct_answer)
+
+#         question_bank.add_question(
+#             question_id, question_content, options, correct_answer_index
+#         )
+
+#     # Process Students' Results
+#     students = DataProcessing().result_file_process(file_path)
+
+#     # Process Exam Data
+#     exam_df = pd.read_csv(os.path.join(UPLOAD_FOLDER, "mock_exam_items.csv"))
+#     exam_data = exam_df.to_dict(orient="records")
+#     exams = process_exam(exam_data, question_bank)
+
+#     # Generate Exam Results and Analysis
+#     exam_result = ExamResult(exams, students)
+#     analysis = CttAnalysis(exam_result)
+
+#     return analysis.analyze_questions_ctt()
+
+
+# def process_exam(file_path, question_bank):
+#     # Group data by Exam_code
+#     file_path.sort(key=lambda x: x["Exam_code"])
+
+#     # Group the data by 'Exam_code'
+#     grouped = {
+#         key: list(file_path)
+#         for key, group in groupby(file_path, key=lambda x: x["Exam_code"])
+#     }
+
+#     exams = []
+
+#     for exam_code, group in grouped.items():
+#         question_order = []
+#         answer_order = {}
+
+#         for question in group:
+#             content = question["Content"]
+#             options = [
+#                 question["Option_A"],
+#                 question["Option_B"],
+#                 question["Option_C"],
+#                 question["Option_D"],
+#             ]
+#             correct_option = question["Correct_Option"]
+
+#             # Match content with the question bank
+#             matched_question_id = None
+#             matched_answer_order = [None] * 4
+
+#             for question_id, question_data in question_bank.get_all_questions().items():
+#                 if question_data["content"] == content:
+#                     matched_question_id = question_id
+#                     matched_answer_order = question_data["options"]
+#                     # for idx, option in enumerate(options):
+#                     #     # if option in question_data['options']:
+#                     #     matched_answer_order[idx] = question_data['options'].index(option)
+#                     # break
+
+#             if matched_question_id is None:
+#                 print(
+#                     f"Warning: Question not found in question bank for Exam Code {exam_code}: {content}"
+#                 )
+#                 continue
+
+#             # Append question order and answer order
+#             question_order.append(matched_question_id)
+#             answer_order[matched_question_id] = matched_answer_order
+
+#         # Initialize Exam object
+#         exam = Exam(
+#             code=exam_code,
+#             question_bank=question_bank,
+#             question_order=question_order,
+#             answer_order=answer_order,
+#         )
+
+#         exams.append(exam)
+
+#     return exams
+
+
 import os
 from itertools import groupby
 
 import pandas as pd
 
+from analysis.ctt_analysis import CttAnalysis
 from config import UPLOAD_FOLDER
 from models.exam import Exam
 from models.exam_result import ExamResult
 from models.question import Option, QuestionBank
 from utils.data_processing import DataProcessing
 from utils.file_handling import save_uploaded_file
-from analysis.ctt_analysis import CttAnalysis
 
 
-def analyze_uploaded_file(file):
-    # Save and process the uploaded file
-    file_path = save_uploaded_file(file)
+class CttService:
+    def __init__(self):
+        self.analysis = None
+        print("CttService initialized.")
 
-    # Load Question Bank
-    question_bank = QuestionBank()
-    exam_df = pd.read_csv(os.path.join(UPLOAD_FOLDER, "mock_exam_items.csv"))
-    exam_data = exam_df.to_dict(orient="records")
-    question_id = 0
-    exam_code = exam_data[1]["Exam_code"]
+    def analyze_uploaded_file(self, file):
+        """
+        Process the uploaded file, initialize the exam results, and perform CTT analysis.
+        """
+        file_path = save_uploaded_file(file)
+        question_bank = self._load_question_bank()
+        students = DataProcessing().result_file_process(file_path)
 
-    for question in exam_data:
-        if question["Exam_code"] == exam_code:
-            question_id = question_id + 1
-            question_content = question["Content"]
-            options = [
-                Option(question["Option_A"]),
-                Option(question["Option_B"]),
-                Option(question["Option_C"]),
-                Option(question["Option_D"]),
-            ]
-            correct_answer = question["Correct_Option"]
-            correct_answer_index = ["A", "B", "C", "D"].index(correct_answer)
+        exam_df = pd.read_csv(os.path.join(UPLOAD_FOLDER, "mock_exam_items.csv"))
+        exams = self._process_exam(exam_df.to_dict(orient="records"), question_bank)
 
-            question_bank.add_question(
-                question_id, question_content, options, correct_answer_index
-            )
-        else:
-            break
+        exam_result = ExamResult(exams, students)
+        self.analysis = CttAnalysis(exam_result)
+        self.analysis.analyze_questions_ctt()
 
-    # Process Students' Results
-    students = DataProcessing().result_file_process(file_path)
+    def get_analysis_results(self):
+        """
+        Retrieve the analysis results.
+        """
+        if self.analysis:
+            return self.analysis.question_stats
+        raise ValueError("CTT analysis not initialized.")
 
-    # Process Exam Data
-    exams = process_exam(exam_data, question_bank)
+    def get_general_detail(self):
+        """
+        Retrieve general details from the CTT analysis.
+        """
+        if self.analysis:
+            return self.analysis.get_general_detail()
+        raise ValueError("CTT analysis not initialized.")
 
-    # Generate Exam Results and Analysis
-    exam_result = ExamResult(exams, students)
-    analysis = CttAnalysis(exam_result)
+    def get_question_stats(self, question_id):
+        """
+        Retrieve stats for a specific question.
+        """
+        if not self.analysis:
+            raise ValueError("CTT analysis not initialized.")
 
-    return analysis.analyze_questions_ctt()
+        sorted_students, top_students, bottom_students = self.analysis._split_students()
+        all_questions = self.analysis.examResult.exams[
+            0
+        ].question_bank.get_all_questions()
+        question_data = all_questions.get(question_id)
 
+        if not question_data:
+            raise ValueError(f"Question with ID {question_id} not found.")
 
-def process_exam(file_path, question_bank):
-    # Group data by Exam_code
-    file_path.sort(key=lambda x: x["Exam_code"])
-
-    # Group the data by 'Exam_code'
-    grouped = {
-        key: list(file_path)
-        for key, group in groupby(file_path, key=lambda x: x["Exam_code"])
-    }
-
-    exams = []
-
-    for exam_code, group in grouped.items():
-        question_order = []
-        answer_order = {}
-
-        for question in group:
-            content = question["Content"]
-            options = [
-                question["Option_A"],
-                question["Option_B"],
-                question["Option_C"],
-                question["Option_D"],
-            ]
-            correct_option = question["Correct_Option"]
-
-            # Match content with the question bank
-            matched_question_id = None
-            matched_answer_order = [None] * 4
-
-            for question_id, question_data in question_bank.get_all_questions().items():
-                if question_data["content"] == content:
-                    matched_question_id = question_id
-                    matched_answer_order = question_data["options"]
-                    # for idx, option in enumerate(options):
-                    #     # if option in question_data['options']:
-                    #     matched_answer_order[idx] = question_data['options'].index(option)
-                    # break
-
-            if matched_question_id is None:
-                print(
-                    f"Warning: Question not found in question bank for Exam Code {exam_code}: {content}"
-                )
-                continue
-
-            # Append question order and answer order
-            question_order.append(matched_question_id)
-            answer_order[matched_question_id] = matched_answer_order
-
-        # Initialize Exam object
-        exam = Exam(
-            code=exam_code,
-            question_bank=question_bank,
-            question_order=question_order,
-            answer_order=answer_order,
+        return self.analysis._analyze_single_question(
+            question_id, question_data, sorted_students, top_students, bottom_students
         )
 
-        exams.append(exam)
+    def get_average_indexes(self):
+        """
+        Retrieve average indexes (difficulty, discrimination, etc.).
+        """
+        if self.analysis:
+            return self.analysis.average_indexes
+        raise ValueError("CTT analysis not initialized.")
 
-    return exams
+    def _load_question_bank(self):
+        """
+        Load question bank data from a predefined file.
+        """
+        question_bank = QuestionBank()
+        question_bank_df = pd.read_csv(
+            os.path.join(UPLOAD_FOLDER, "mock_exam_items.csv")
+        )
+        question_bank_df = question_bank_df.groupby('Exam_code')
+        question_bank_df = question_bank_df.to_dict(orient="records")
+        question_id = 0
+        exam_code = question_bank_df[1]["Exam_code"]
+        for question in question_bank_df.to_dict(orient="records"):
+            if question["Exam_code"] == exam_code:
+                question_id = question_id + 1
+                correct_answer_index = ["A", "B", "C", "D"].index(
+                    question["Correct_Option"]
+                )
+                options = [
+                    Option(question["Option_A"]),
+                    Option(question["Option_B"]),
+                    Option(question["Option_C"]),
+                    Option(question["Option_D"]),
+                ]
+                question_bank.add_question(
+                    question_id,
+                    question["Content"],
+                    options,
+                    correct_answer_index,
+                )
+            else:
+                break
+        return question_bank
+
+    def _process_exam(self, exam_data, question_bank):
+        """
+        Process exam data and group by Exam Code.
+        """
+        exam_data.sort(key=lambda x: x["Exam_code"])
+        grouped = {
+            key: list(group)
+            for key, group in groupby(exam_data, key=lambda x: x["Exam_code"])
+        }
+
+        exams = []
+        for exam_code, group in grouped.items():
+            question_order = []
+            answer_order = {}
+
+            for question in group:
+                matched_question = next(
+                    (
+                        q_id
+                        for q_id, q_data in question_bank.get_all_questions().items()
+                        if q_data["content"] == question["Content"]
+                    ),
+                    None,
+                )
+                if not matched_question:
+                    print(
+                        f"Warning: Question not found for Exam Code {exam_code}: {question['Content']}"
+                    )
+                    continue
+                question_order.append(matched_question)
+                answer_order[matched_question] = question_bank.get_all_questions()[
+                    matched_question
+                ]["options"]
+
+            exams.append(Exam(exam_code, question_bank, question_order, answer_order))
+
+        return exams
