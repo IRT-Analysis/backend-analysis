08a06ee96a8e6851378a4c52894c657617692fb7
longtruong2604 <longtruong2604@gmail.com>
2025-02-17 22:05:40 +0700

    feat: set up logging

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from flask import Flask
 from routes.ctt_analyze import ctt_analyze
 from utils.error_handlers import register_error_handlers
 
+import logging
+
+# Configure logging
+logging.basicConfig(
+    filename="app.log",  # Specify the log file name
+    filemode="a",  # Append mode ('a' for append, 'w' for overwrite)
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+)
+
+
 app = Flask(__name__)
 
 # Load configurations
