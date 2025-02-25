3c7b97af68dfa9b23b93c4250085ef9e87188cb3
thtienn <thtienn12@gmail.com>
2025-02-25 09:42:05 +0700

    restructure

--- analysis/rasch_analysis.py
+++ analysis/rasch_analysis.py
@@ -5,7 +5,7 @@ from scipy.optimize import minimize
 from models.student import Student
 
 
-class IrtAnalysis(Model):
+class RaschAnalysis(Model):
     response_list = []
     prob_list = []
     
@@ -15,69 +15,6 @@ class IrtAnalysis(Model):
         self.average_indexes = {}
         self.question_stats = {}
         
-    # def analyze_questions_irt(self):
-    #     """
-    #     Main function to analyze questions using IRT model.
-    #     """
-    #     all_questions = self.examResult.exams[0].question_bank.get_all_questions()
-    #     total_students = len(self.examResult.students)
-    #     self.general_detail.update({
-    #         "total_students": total_students,
-    #         "total_questions": len(all_questions)
-    #     })
-
-    #     sorted_students, top_students, bottom_students = self.split_students()
-
-    #     question_stats_list = [
-    #         self._analyze_single_question_irt(
-    #             question_id,
-    #             question_data,
-    #             sorted_students,
-    #             top_students,
-    #             bottom_students,
-    #             difficulty=0.5,  # Example value
-    #             discrimination=1.0  # Example value
-    #         ) for question_id, question_data in all_questions.items()
-    #     ]
-
-    #     self.average_indexes.update({
-    #         "average_score": self.get_average_value("score", self.examResult.scores),
-    #         "average_discrimination": self.get_average_value("discrimination", question_stats_list),
-    #         "average_difficulty": self.get_average_value("difficulty", question_stats_list),
-    #         # "average_rpbis": self.get_average_value("rpbis", question_stats_list)
-    #     })
-
-    #     self.question_stats.update({
-    #         question_id: stat for question_id, stat in zip(all_questions.keys(), question_stats_list)
-    #     })
-
-    #     return self.question_stats
-
-    # def _analyze_single_question_irt(self, question_id, question_data, sorted_students, top_students, bottom_students, difficulty=0.5, discrimination=1.0):
-    #     """
-    #     Analyze a single question using IRT model.
-    #     """
-    #     response_data = self._get_response_data(question_id, sorted_students)
-    #     # response_data_top = self._get_response_data(question_id, top_students)
-    #     # response_data_bottom = self._get_response_data(question_id, bottom_students)
-
-    #     # Fit the IRT model
-    #     model = two_parameter_model(0.5, 0.5, 0.5)
-    #     model.fit(response_data, difficulty, discrimination)
-
-    #     # Calculate the discrimination and difficulty for the question
-    #     discrimination, difficulty = model.get_params()
-
-    #     # Calculate the RPBIS
-    #     # r_pbis = model.get_rpbis(response_data_top, response_data_bottom)
-
-    #     return {
-    #         "model": model,
-    #         "discrimination": discrimination,
-    #         "difficulty": difficulty,
-    #         # "r_pbis": r_pbis
-    #     }
-
     def _get_response_data(self, question_id, sorted_students):
         """
         Get response data for IRT model fitting.
@@ -222,21 +159,5 @@ class RaschModel:
 
     def calculate_person_outfit(self, prob, response_data):
         return 0
-    
-class Irt2PL:
-    def __init__(self):
-        pass
-    
-    def fit(self, response_data, difficulty=0.5, discrimination=1.0):
-        def likelihood(params, *args):
-            difficulty, discrimination = params
-            response_data = np.array(args[0], dtype=np.float64)
-            prob = 1 / (1 + np.exp(discrimination * (difficulty - response_data)))
-            epsilon = 1e-10
-            likelihood = np.sum(response_data * np.log(prob + epsilon) + (1 - response_data) * np.log(1 - prob + epsilon))
-            return -likelihood
-        initial_params = [difficulty, discrimination]
-        result = minimize(likelihood, initial_params, args=(response_data,), method='BFGS')
-        
-            
+         
     
